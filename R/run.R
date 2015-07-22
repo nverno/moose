b215b5552d891fec0b2274d54def1a9138c2f7d2
Noah Peart <noah.v.peart@gmail.com>
2015-07-21 21:31:09 -0400

    Batch update

--- R/run.R
+++ R/run.R
@@ -1,9 +1,14 @@
 #!/usr/bin/Rscript
 
 ## This
-library(shiny)
-runApp(appDir = "../app")
+## library(shiny)
+## runApp(appDir = "../app", launch.browser = TRUE)
 
 ## Or this
-## rmarkdown::run("Main.Rmd")
-
+rmarkdown::run(
+    default_file = "main.Rmd",
+    dir = "C:/home/work/moose",
+    shiny_args = list(
+        port = 7088,
+        launch.browser = TRUE,
+        host = "127.0.0.1"))
